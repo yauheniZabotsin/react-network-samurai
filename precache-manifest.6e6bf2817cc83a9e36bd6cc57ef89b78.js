ea4830590024329d87eb5c1abbedefe5878dedd5
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-04-17 00:10:50 +0300

    Updates

---
+++ precache-manifest.6e6bf2817cc83a9e36bd6cc57ef89b78.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b586914fea617aebb823",
+    "url": "/react-network-samurai/static/css/main.1c4314cb.chunk.css"
+  },
+  {
+    "revision": "b586914fea617aebb823",
+    "url": "/react-network-samurai/static/js/main.b586914f.chunk.js"
+  },
+  {
+    "revision": "079472fb207fbf6ef83a",
+    "url": "/react-network-samurai/static/css/1.562b9f7e.chunk.css"
+  },
+  {
+    "revision": "079472fb207fbf6ef83a",
+    "url": "/react-network-samurai/static/js/1.079472fb.chunk.js"
+  },
+  {
+    "revision": "1d0079ff7e8ac0c9038c",
+    "url": "/react-network-samurai/static/css/2.f088cff4.chunk.css"
+  },
+  {
+    "revision": "1d0079ff7e8ac0c9038c",
+    "url": "/react-network-samurai/static/js/2.1d0079ff.chunk.js"
+  },
+  {
+    "revision": "0b9299ec73563172f175",
+    "url": "/react-network-samurai/static/js/3.0b9299ec.chunk.js"
+  },
+  {
+    "revision": "e8309efced4c9cfc5588",
+    "url": "/react-network-samurai/static/js/4.e8309efc.chunk.js"
+  },
+  {
+    "revision": "8b5c4f9359c7dfc343be",
+    "url": "/react-network-samurai/static/js/runtime~main.8b5c4f93.js"
+  },
+  {
+    "revision": "9ac917ba2fc278d3acd20c1edd240e91",
+    "url": "/react-network-samurai/static/media/user.9ac917ba.jpg"
+  },
+  {
+    "revision": "d310b63be4826b2224deb39d51740510",
+    "url": "/react-network-samurai/static/media/preloader.d310b63b.svg"
+  },
+  {
+    "revision": "095f36f509d3b9682b4bea63720958ab",
+    "url": "/react-network-samurai/static/media/logo.095f36f5.svg"
+  },
+  {
+    "revision": "079473c01745964aecbc77d04af85444",
+    "url": "/react-network-samurai/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-network-samurai/precache-manifest.6e6bf2817cc83a9e36bd6cc57ef89b78.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-network-samurai/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
