d3700b41534fca9470aa602a1015048c6b1fd0a3
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-09-12 18:35:49 +0300

    feat: typed TS object-helpers file

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -24,10 +24,11 @@ const rootReducer = combineReducers({
   app: appReducer,
 });
 
-type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never;
-export type InferActionsTypes<
-  T extends { [key: string]: (...args: any[]) => any }
-> = ReturnType<PropertiesTypes<T>>;
+export type InferActionsTypes<T> = T extends {
+  [keys: string]: (...args: any[]) => infer U;
+}
+  ? U
+  : never;
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
